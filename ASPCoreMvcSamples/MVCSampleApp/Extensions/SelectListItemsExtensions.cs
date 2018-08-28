617c7218d019d28ec5694e1f759752c0afd5c1e9
Stone <Stone@DESKTOP-V5RCBJA>
2018-08-28 12:14:53 +0800

    MVC

---
+++ ASPCoreMvcSamples/MVCSampleApp/Extensions/SelectListItemsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCSampleApp.Extensions
+{
+    public static class SelectListItemsExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItems(
+            this IDictionary<int, string> dict, int selected) =>
+            dict.Select(item =>
+            new SelectListItem
+            {
+                Selected = item.Key == selected,
+                Text = item.Value,
+                Value = item.Key.ToString()
+            });
+    }
+}
